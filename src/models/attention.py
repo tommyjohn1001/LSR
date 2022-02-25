Add stable softmax and LayerNorm to fix NaN loss

src/models/attention.py
@@ -1,8 +1,22 @@
-import torch.nn as nn
-import torch
-import torch.nn.functional as F
 import copy
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 from models.lockedropout import LockedDropout
+
+from all_packages import *
+
+
+def stable_masked_softmax(x, mask):
+    x = x - x.max()
+    x = x.masked_fill_(mask[:,None] == 0, float("-inf"))
+    x = torch.softmax(x, -1)
+
+    return x
+
 
 class SelfAttention(nn.Module):
     def __init__(self, input_size):
@@ -10,14 +24,36 @@ class SelfAttention(nn.Module):
         self.input_linear = nn.Linear(input_size, 1, bias=False)
         self.dot_scale = nn.Parameter(torch.Tensor(input_size).uniform_(1.0 / (input_size ** 0.5)))
 
+        self.ff1 = nn.Sequential(
+            nn.Linear(120, 120),
+            nn.Tanh(),
+            nn.LayerNorm(120)
+        )
+        self.ff2 = nn.Sequential(
+            nn.Linear(120, 120),
+            nn.Tanh(),
+            nn.LayerNorm(120)
+        )
+
     def forward(self, input, memory, mask):
 
-        input_dot = self.input_linear(input) #nan: cal the weight for the same word
-        cross_dot = torch.bmm(input * self.dot_scale, memory.permute(0, 2, 1).contiguous())
-        att = input_dot + cross_dot
-        att = att - 1e30 * (1 - mask[:,None])
+        input_dot = self.input_linear(input)  # nan: cal the weight for the same word
 
-        weight_one = F.softmax(att, dim=-1)
+        inp = self.ff1(input * self.dot_scale)
+        memory_ = self.ff2(memory).permute(0, 2, 1).contiguous()
+        cross_dot = inp @ memory_
+
+        att = input_dot + cross_dot
+
+        ## Numerically stable version
+        weight_one = stable_masked_softmax(att, mask)
+
+        # att = att - 1e30 * (1 - mask[:,None])
+        # weight_one = F.softmax(att, dim=-1)
+
+        if NaNReporter.check_abnormal(weight_one, "weight_one"):
+            print(f"att: max: {att.max()} - min: {att.min()}")
+
         output_one = torch.bmm(weight_one, memory)
 
         return torch.cat([input, output_one], dim=-1)
@@ -42,14 +78,15 @@ class BiAttention(nn.Module):
         memory_dot = self.memory_linear(memory).view(bsz, 1, memory_len)
         cross_dot = torch.bmm(input * self.dot_scale, memory.permute(0, 2, 1).contiguous())
         att = input_dot + memory_dot + cross_dot
-        att = att - 1e30 * (1 - mask[:,None])
+        att = att - 1e30 * (1 - mask[:, None])
 
         weight_one = F.softmax(att, dim=-1)
         output_one = torch.bmm(weight_one, memory)
         weight_two = F.softmax(att.max(dim=-1)[0], dim=-1).view(bsz, 1, input_len)
         output_two = torch.bmm(weight_two, input)
 
-        return torch.cat([input, output_one, input*output_one, output_two*output_one], dim=-1)
+        return torch.cat([input, output_one, input * output_one, output_two * output_one], dim=-1)
+
 
 def attention(query, key, mask=None, dropout=None):
     d_k = query.size(-1)
@@ -67,8 +104,8 @@ def attention(query, key, mask=None, dropout=None):
 def clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
 
-class MultiHeadAttention(nn.Module):
 
+class MultiHeadAttention(nn.Module):
     def __init__(self, h, d_model, dropout=0.1):
         super(MultiHeadAttention, self).__init__()
         assert d_model % h == 0
@@ -84,8 +121,10 @@ class MultiHeadAttention(nn.Module):
 
         nbatches = query.size(0)
 
-        query, key = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
-                             for l, x in zip(self.linears, (query, key))]
+        query, key = [
+            l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+            for l, x in zip(self.linears, (query, key))
+        ]
         attn = attention(query, key, mask=mask, dropout=self.dropout)
 
         return attn